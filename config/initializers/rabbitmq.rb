3ccdde9f617959a0a685223f8262b80b30a129db
konradrogala <konrad.rogala@visma.com>
2023-04-28 16:00:16 +0200

    consume-logs

---
+++ config/initializers/rabbitmq.rb
@@ -0,0 +1,16 @@
+require "bunny"
+
+connection_options =
+  {
+    host: "localhost",
+    port: "5672",
+    vhost: "/",
+    username: "guest",
+    password: "guest"
+  }
+
+connection = Bunny.new(connection_options).tap(&:start)
+channel = connection.create_channel
+
+book_loans_queue = channel.queue("basic_app_book_loans") #tworzenie kolejki
+book_loans_queue.bind("basic_app") #podpięcie pod exchange
