3f5c5dd100af981bac939cb0f45a9e44784bbce4
konradrogala <konrad.rogala@visma.com>
2023-05-08 20:52:53 +0200

    working consumer

--- config/initializers/rabbitmq.rb
+++ config/initializers/rabbitmq.rb
@@ -1,16 +1,16 @@
-require "bunny"
+# require "bunny"
 
-connection_options =
-  {
-    host: "localhost",
-    port: "5672",
-    vhost: "/",
-    username: "guest",
-    password: "guest"
-  }
+# connection_options =
+#   {
+#     host: "localhost",
+#     port: "5672",
+#     vhost: "/",
+#     username: "guest",
+#     password: "guest"
+#   }
 
-connection = Bunny.new(connection_options).tap(&:start)
-channel = connection.create_channel
+# connection = Bunny.new(connection_options).tap(&:start)
+# channel = connection.create_channel
 
-book_loans_queue = channel.queue("basic_app_book_loans") #tworzenie kolejki
-book_loans_queue.bind("basic_app") #podpięcie pod exchange
+# book_loans_queue = channel.queue("basic_app_book_loans") #tworzenie kolejki
+# book_loans_queue.bind("basic_app") #podpięcie pod exchange
